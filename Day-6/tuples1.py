f2527bc03f5f05928e1ed71d0f5e7c83f1613a55
ahmedbahattab <ahmedbahattab10@gmail.com>
2025-08-20 23:32:33 +0530

    Create tuples1.py

---
+++ Day-6/tuples1.py
@@ -0,0 +1,9 @@
+# Syntax
+tpl = ('item1', 'item2', 'item3')
+first_item = tpl[0]
+second_item = tpl[1]
+fruits = ('banana', 'orange', 'mango', 'lemon')
+first_fruit = fruits[0]
+second_fruit = fruits[1]
+last_index =len(fruits) - 1
+last_fruit = fruits[las_index]
