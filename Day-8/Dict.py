511c99d71d1161810de4d262e09dc66fa4110d78
ahmedbahattab <ahmedbahattab10@gmail.com>
2025-08-23 23:55:10 +0530

    Create Dict.py

---
+++ Day-8/Dict.py
@@ -0,0 +1,16 @@
+# syntax
+empty_dict = {}
+# Dictionary with data values
+dct = {'key1':'value1', 'key2':'value2', 'key3':'value3', 'key4':'value4'}
+person = {
+    'first_name':'Asabeneh',
+    'last_name':'Yetayeh',
+    'age':250,
+    'country':'Finland',
+    'is_marred':True,
+    'skills':['JavaScript', 'React', 'Node', 'MongoDB', 'Python'],
+    'address':{
+        'street':'Space street',
+        'zipcode':'02210'
+    }
+    }
